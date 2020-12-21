d17c6dd22c52a5033f52a56c3bd75187ea82f557
Jason Jacobs <nerfnerd@gmail.com>
2020-12-20 16:51:22 -0800

    Configure for gmail.

--- app/mailers/email_link_mailer.rb
+++ app/mailers/email_link_mailer.rb
@@ -3,13 +3,13 @@ class EmailLinkMailer < ApplicationMailer
     @email_link = email_link
     @user = email_link.user
 
-    mail to: @user.email, subject: "Here is your login link! 🚀"
+    mail to: @user.email, subject: "Here is your login link!"
   end
 
   def welcome_mail(email_link, user)
     @email_link = email_link
     @user = user
 
-    mail to: @user.email, subject: "Welcome to UniFiDAO! 🚀"
+    mail to: @user.email, subject: "Welcome to UniFiDAO!"
   end
 end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -60,6 +60,19 @@ Rails.application.configure do
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 
-  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
-  config.action_mailer.delivery_method = :letter_opener
+ config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  # config.action_mailer.delivery_method = :letter_opener
+  config.action_mailer.delivery_method = :smtp
+  # SMTP settings for gmail
+  smtp_settings =  {
+          address: "smtp.gmail.com",
+          port: 587,
+          domain: "unifidao.vc",
+          authentication: "plain",
+          enable_starttls_auto: true,
+          user_name: ENV["GMAIL_USERNAME"],
+          password: ENV["GMAIL_PASSWORD"]
+      }
+  config.action_mailer.smtp_settings = smtp_settings
+
 end
