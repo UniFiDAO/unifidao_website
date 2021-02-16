31d28e82e74c2ec81ab211161adf7f3faa57a3a3
Jason Jacobs <nerfnerd@gmail.com>
2021-02-16 15:55:07 -0800

    only allow admins to view admin pages.. if not send to root path

--- app/controllers/admins_controller.rb
+++ app/controllers/admins_controller.rb
@@ -2,6 +2,8 @@ class AdminsController < ApplicationController
   before_action :set_admin, only: [:show, :edit, :update, :destroy]
 
   def users
+    redirect_back fallback_location: root_path unless current_user_admin?
+
     @users = params[:type].nil? ? User.team : User.where(user_type: params[:type])
 
     respond_to do |format|
@@ -11,14 +13,20 @@ class AdminsController < ApplicationController
   end
 
   def events
+    redirect_back fallback_location: root_path unless current_user_admin?
+
     @events = Event.all
   end
 
   def teams
+    redirect_back fallback_location: root_path unless current_user_admin?
+
     @teams = Team.all
   end
 
   def download_investors
+    redirect_back fallback_location: root_path unless current_user_admin?
+
     @investor
   end
 end
